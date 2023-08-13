ac8f447b9bace7b1abda845ac63abd6bbd2de780
Mohamed <mohamed.nour2619@outlook.com>
2023-08-13 19:51:26 +0300

    “commit”

---
+++ README.md
@@ -0,0 +1,17 @@
+Admin_Management
+User Login and Registration features:
+User registration with email User Login with remember password
+. Change password
+. User profile
+. User profile edit save
+. Admin Panel features: Admin login
+. Admin password Chane password. Admin profile
+. Dashboard View Users list. Add new user with role
+. Edit save user
+. Delete user.
+
+How to Install
+Create a database name (db_admin)
+Import database file (db_admin.sql)
+Admin username: mohamed.nour2619@outlook.com
+Admin pass: Phoenix1
---
+++ db_admin.sql
@@ -0,0 +1,44 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+DROP TABLE IF EXISTS `tbl_product`;
+CREATE TABLE IF NOT EXISTS `tbl_product` (
+  `Name` varchar(50) NOT NULL,
+  `Prix` int(11) NOT NULL,
+  `Categorie` varchar(50) NOT NULL,
+  `etat` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `tbl_roles`;
+CREATE TABLE IF NOT EXISTS `tbl_roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'role_id',
+  `role` varchar(255) DEFAULT NULL COMMENT 'role_text',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `tbl_roles` (`id`, `role`) VALUES
+(1, 'Admin'),
+(2, 'Editor'),
+(3, 'User');
+
+DROP TABLE IF EXISTS `tbl_users`;
+CREATE TABLE IF NOT EXISTS `tbl_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `mobile` varchar(25) DEFAULT NULL,
+  `roleid` tinyint(4) DEFAULT NULL,
+  `isActive` tinyint(4) DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO `tbl_users` (`id`, `name`, `username`, `email`, `password`, `mobile`, `roleid`, `isActive`, `created_at`, `updated_at`) VALUES
+COMMIT;
